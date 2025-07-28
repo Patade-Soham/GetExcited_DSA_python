d8e37ea08c28da7e7ba70a1af0fe1503a4a6d0fe
Soham Patade <sohampatade30@eng.rizvi.edu.in>
2025-07-29 01:35:01 +0530

    Create selectionSort.py

---
+++ selectionSort.py
@@ -0,0 +1,11 @@
+def selectionSort(array):
+    n=len(array)
+    for i in range(n):
+        min_index=i
+        for j in range(i+1,n):
+            if array[j]<array[min_index]:
+                min_index=j
+        array[i],array[min_index]=array[min_index],array[i]
+
+        
+    return array

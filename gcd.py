648f2cea5d763560728cd3e195c701c58d38ed64
Soham Patade <sohampatade30@eng.rizvi.edu.in>
2025-09-15 03:00:43 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,18 +1,8 @@
-def factor(n):
-    lis=[]
-    for i in range(1,n+1):
-        if n%i==0:
-            lis.append(i)
-        
-    return lis    
-        
 def gcd(n,m):
-    lis1=factor(n)
-    lis2=factor(m)
-    lis3=[]
-    for i in range(len(lis1)-1):
-        if lis1[i]  in lis2:
-            lis3.append(lis1[i])
-    return lis3[-1]
-    
-print(gcd(12,8))
+
+    for i in range(1,min(m,n)+1):
+        if n%i==0 and m%i==0 :
+            num=i
+        
+    return num
+print(gcd(12,144))
